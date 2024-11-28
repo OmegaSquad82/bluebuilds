dd5432f2104288618a74e653c3562c750f3c456d
OmegaSquad82 <34405234+OmegaSquad82@users.noreply.github.com>
2024-11-28 19:45:58 +0100

    Add https://crates.io/crates/zram-generator variable resolving stuff (why does cargo not correctly do it? ;c)

--- files/scripts/setup-zram.sh
+++ files/scripts/setup-zram.sh
@@ -2,14 +2,14 @@
 ## devhints.io/bash
 set -euo pipefail
 
-# cd /tmp
-# git clone https://github.com/systemd/zram-generator.git
-# cd zram-generator
+# https://crates.io/crates/zram-generator
+SYSTEMD_UTIL_DIR := $pkg-config --variable=systemdutildir systemd)
+SYSTEMD_SYSTEM_UNIT_DIR := $pkg-config --variable=systemdsystemunitdir systemd)
+SYSTEMD_SYSTEM_GENERATOR_DIR := $pkg-config --variable=systemdsystemgeneratordir systemd)
 
-# make build
-# make install NOBUILD=true
-# make clean
-
+export SYSTEMD_UTIL_DIR
+export SYSTEMD_SYSTEM_UNIT_DIR
+export SYSTEMD_SYSTEM_GENERATOR_DIR
 cargo install zram-generator # >=v1.2.1
 
 sed -i 's/.*algorithm.*/compression-algorithm = lz4 zstd lc4hc/g' /etc/systemd/zram-generator.conf
