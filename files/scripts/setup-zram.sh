093a8bea29a5ce009055afec5698e9a6fb10d929
OmegaSquad82 <34405234+OmegaSquad82@users.noreply.github.com>
2024-11-28 19:31:41 +0100

    Use crate to install zram-generator

--- files/scripts/setup-zram.sh
+++ files/scripts/setup-zram.sh
@@ -2,12 +2,14 @@
 ## devhints.io/bash
 set -euo pipefail
 
-cd /tmp
-git clone https://github.com/systemd/zram-generator.git
-cd zram-generator
+# cd /tmp
+# git clone https://github.com/systemd/zram-generator.git
+# cd zram-generator
 
-make build
-make install NOBUILD=true
-make clean
+# make build
+# make install NOBUILD=true
+# make clean
+
+cargo install zram-generator # >=v1.2.1
 
 sed -i 's/.*algorithm.*/compression-algorithm = lz4 zstd lc4hc/g' /etc/systemd/zram-generator.conf
