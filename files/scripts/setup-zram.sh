68ebe2c9de40a2bb1d7f66c5baf928ec4fbcf8ce
OmegaSquad82 <34405234+OmegaSquad82@users.noreply.github.com>
2024-11-28 01:10:23 +0100

    Add dependencies to generate man pages

--- files/scripts/setup-zram.sh
+++ files/scripts/setup-zram.sh
@@ -2,9 +2,13 @@
 ## devhints.io/bash
 set -euo pipefail
 
+# dependencies to build man pages
+gem install hpricot mustache rdiscount ronn
+
 cd /tmp
 git clone https://github.com/systemd/zram-generator.git
 cd zram-generator
+
 make build
 make install NOBUILD=true
 make clean
