6ad787e3b4d3a9dff90edb6f90d193370bacadba
OmegaSquad82 <34405234+OmegaSquad82@users.noreply.github.com>
2024-11-29 19:43:29 +0100

    Fix rust toolchain path and fail early on image build

--- files/scripts/setup-zram.sh
+++ files/scripts/setup-zram.sh
@@ -5,7 +5,9 @@ set -euo pipefail
 
 # https://www.rust-lang.org/
 curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y
-export PATH="~/.cargo/bin;$PATH"
+
+# verify installation
+. "$HOME"/.cargo/env
 rustup --version
 rustc --version
 cargo --version
@@ -15,11 +17,11 @@ export SYSTEMD_UTIL_DIR=$(pkg-config --variable=systemdutildir systemd)
 export SYSTEMD_SYSTEM_UNIT_DIR=$(pkg-config --variable=systemdsystemunitdir systemd)
 export SYSTEMD_SYSTEM_GENERATOR_DIR=$(pkg-config --variable=systemdsystemgeneratordir systemd)
 cargo install zram-generator # >=v1.2.1 supports recompression and options for algorithms
-install -b ~/.cargo/bin/zram-generator "$SYSTEMD_SYSTEM_GENERATOR_DIR"
+install -b "$HOME"/.cargo/bin/zram-generator "$SYSTEMD_SYSTEM_GENERATOR_DIR"
 
 # cleanup
 rustup self uninstall -y
-rm -rf ~/.cargo/
+rm -rf "$HOME"/.cargo/
 
 # set (re)compression algorithms for all configured zram devices
 sed -i 's/^compression-.*/compression-algorithm = lz4 zstd lz4hc (type=idle)/g' /etc/systemd/zram-generator.conf
