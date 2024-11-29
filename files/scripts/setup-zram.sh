f015c7f4e570c669bd6d99aa68a23c4d9371c690
OmegaSquad82 <34405234+OmegaSquad82@users.noreply.github.com>
2024-11-29 01:00:12 +0100

    systemd/zram-generator and recompression Fixes #60

--- files/scripts/setup-zram.sh
+++ files/scripts/setup-zram.sh
@@ -10,7 +10,8 @@ SYSTEMD_SYSTEM_GENERATOR_DIR=$(pkg-config --variable=systemdsystemgeneratordir s
 export SYSTEMD_UTIL_DIR
 export SYSTEMD_SYSTEM_UNIT_DIR
 export SYSTEMD_SYSTEM_GENERATOR_DIR
-cargo install zram-generator --root "$SYSTEMD_SYSTEM_GENERATOR_DIR" # >=v1.2.1
+cargo install zram-generator # >=v1.2.1
+install -b /root/.cargo/bin/zram-generator "$SYSTEMD_SYSTEM_GENERATOR_DIR"
 rm -rf /root/.cargo/
 
 sed -i 's/.*algorithm.*/compression-algorithm = lz4 zstd lc4hc/g' /etc/systemd/zram-generator.conf
