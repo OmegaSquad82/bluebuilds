c36138388be2f4ed165e0ac5bced7191c3248aa2
OmegaSquad82 <34405234+OmegaSquad82@users.noreply.github.com>
2024-11-29 19:32:13 +0100

    Streamline zram-generator build remove toolchain afterwards reinstate previous recipes

--- files/scripts/setup-zram.sh
+++ files/scripts/setup-zram.sh
@@ -1,17 +1,25 @@
 #!/usr/bin/env bash
+
 ## devhints.io/bash
 set -euo pipefail
 
+# https://www.rust-lang.org/
+curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y
+export PATH="~/.cargo/bin;$PATH"
+rustup --version
+rustc --version
+cargo --version
+
 # https://crates.io/crates/zram-generator
-SYSTEMD_UTIL_DIR=$(pkg-config --variable=systemdutildir systemd)
-SYSTEMD_SYSTEM_UNIT_DIR=$(pkg-config --variable=systemdsystemunitdir systemd)
-SYSTEMD_SYSTEM_GENERATOR_DIR=$(pkg-config --variable=systemdsystemgeneratordir systemd)
+export SYSTEMD_UTIL_DIR=$(pkg-config --variable=systemdutildir systemd)
+export SYSTEMD_SYSTEM_UNIT_DIR=$(pkg-config --variable=systemdsystemunitdir systemd)
+export SYSTEMD_SYSTEM_GENERATOR_DIR=$(pkg-config --variable=systemdsystemgeneratordir systemd)
+cargo install zram-generator # >=v1.2.1 supports recompression and options for algorithms
+install -b ~/.cargo/bin/zram-generator "$SYSTEMD_SYSTEM_GENERATOR_DIR"
 
-export SYSTEMD_UTIL_DIR
-export SYSTEMD_SYSTEM_UNIT_DIR
-export SYSTEMD_SYSTEM_GENERATOR_DIR
-cargo install zram-generator # >=v1.2.1
-install -b /root/.cargo/bin/zram-generator "$SYSTEMD_SYSTEM_GENERATOR_DIR"
-rm -rf /root/.cargo/
+# cleanup
+rustup self uninstall -y
+rm -rf ~/.cargo/
 
-sed -i 's/.*algorithm.*/compression-algorithm = lz4 zstd lc4hc/g' /etc/systemd/zram-generator.conf
+# set (re)compression algorithms for all configured zram devices
+sed -i 's/^compression-.*/compression-algorithm = lz4 zstd lz4hc (type=idle)/g' /etc/systemd/zram-generator.conf
